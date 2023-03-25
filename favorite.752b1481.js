169a8d1056b13ad666202bb0559dd889e8659f26
TikhonenkoVV <113926273+TikhonenkoVV@users.noreply.github.com>
2023-03-25 16:53:33 +0000

    Deploying to gh-pages from @ TikhonenkoVV/news_app@3e22ae972a2343d2e81cf60f82d05dbae8be8e8d 🚀

--- favorite.752b1481.js
+++
@@ -1,2 +0,0 @@
-({togler:document.querySelector(".theme__togler"),newsContainer:document.querySelector(".news")}).togler.addEventListener("click",(e=>{document.body.classList.toggle("dark")}));
-//# sourceMappingURL=favorite.752b1481.js.map
--- favorite.99d20ab9.js
+++
@@ -1,2 +0,0 @@
-[document.querySelector(".theme__togler"),document.querySelector(".news")][0].addEventListener("click",(function(e){document.body.classList.toggle("dark")}));
-//# sourceMappingURL=favorite.99d20ab9.js.map
